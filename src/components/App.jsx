f74c3729a8c6ebffeedc9cfa617ab711a576e110
meganpaffrath <meganpaffrath@gmail.com>
2021-01-18 21:10:19 -0800

    searchable recipes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import recipes from "../recipes.json";
 import Recipe from "./Recipe";
 
 export default function App() {
+  const [type, setType] = useState("all");
+
   function makeRecipe(recipe) {
     const ovenTemp = recipe.ovenTemp ? recipe.ovenTemp : 0
 
@@ -11,15 +13,69 @@ export default function App() {
       recipe={recipe.recipe}
       ingredients={recipe.ingredients}
       instructions={recipe.instructions}
+      categories={recipe.categories}
       ovenTemp={ovenTemp}
     />
+  }
+
+  function filterRecipes(recipes, search) {
+    const recipesNew = recipes.filter(function(recipe) {
+      // Find by name
+      if (recipe.recipe.toLowerCase().includes(search.toLowerCase())) {
+        return true;
+      }
+      // Find by Ingredient
+      for (const ingredient in recipe.ingredients) {
+        if (recipe.ingredients[ingredient].toLowerCase().includes(search.toLowerCase())) {
+          return true;
+        }
+      }
+
+      // Find by type
+      for (const type in recipe.categories) {
+        if (recipe.categories[type].toLowerCase().includes(search.toLowerCase())) {
+          return true;
+        }
+      }
+
+      // find by temp
+      if (recipe.ovenTemp === search) {
+        return true;
+      }
+
+      // Otherwise
+      return false;
+    });
+    
+    return recipesNew;
+  }
+
+  function handleChange(input) {
+    setType(input.target.value);
   }
 
 
   return (
     <div>
       <h1>Megan's Recipes</h1>
-      {recipes.map(makeRecipe)}
+      <br/>
+      <center>
+      <input onChange={handleChange} type="search" id="mySearch" placeholder="Search by title/ingredient/type/temperature" size="40"/>
+      </center>
+      <br/>
+      <center>
+        <button onClick={() => setType("meal")}>meals</button>
+        <button onClick={() => setType("snack")}>snacks</button>
+        <button onClick={() => setType("all")}>all</button>
+      </center>
+
+      <br/>
+      
+
+      {type === "all" ? (
+        recipes.map(makeRecipe)
+      ) : filterRecipes(recipes, type).map(makeRecipe)}
+
     </div>
     
   )
--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Recipe({recipe, ingredients, instructions, ovenTemp}) {
+export default function Recipe({recipe, ingredients, instructions, ovenTemp, categories}) {
   var keyIng = 0;
   var keyInst = 0;
   return (
@@ -26,6 +26,10 @@ export default function Recipe({recipe, ingredients, instructions, ovenTemp}) {
         }
         )}
       </ol>
+      <h2>Categories</h2>
+      <p>{categories.map((category) => {
+        return category + " "
+      })}</p>
       <h3>Temp: {ovenTemp}</h3>
     </div>
     
